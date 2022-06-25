8e0671d08f568b2c56b7b4c5bd3bafcfa1561b21
a.kazakov <a.kazakov@bidzaar.com>
2022-06-25 16:10:43 +0300

    Fix code

--- generate_string.c
+++ generate_string.c
@@ -4,7 +4,7 @@
 int main(int argc, char const *argv[])
 {
 
-    if (argc != 2) 
+    if (argc != 2)
     {
         printf("Missing string length parameter");
         return 1;
@@ -12,11 +12,11 @@ int main(int argc, char const *argv[])
 
     char c = 'D';
     char *p_str = NULL;
-    
-    int length_str = atoi(argv[1]);
-    p_str = (char *) malloc(length_str * sizeof(char));
 
-    for(int i = 0; i < length_str; ++i)
+    int length_str = atoi(argv[1]);
+    p_str = (char *)malloc(length_str * sizeof(char));
+
+    for (int i = 0; i < length_str; ++i)
     {
         p_str[i] = c;
         printf("%c", p_str[i]);
