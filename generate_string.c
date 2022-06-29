4e91c46e27131a975ec705003cffb78aa1ff9837
a.kazakov <a.kazakov@bidzaar.com>
2022-06-29 20:46:42 +0300

    Add function generate_random_simbol

--- generate_string.c
+++ generate_string.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char generate_random_simbol(int i);
+
 int main(int argc, char const *argv[])
 {
 
     if (argc != 2)
     {
-        printf("Missing string length parameter");
+        printf("Missing string length parameter.");
         return 1;
     }
 
-    char c = 'D';
     char *p_str = NULL;
 
     int length_str = atoi(argv[1]);
@@ -18,12 +19,18 @@ int main(int argc, char const *argv[])
 
     for (int i = 0; i < length_str; ++i)
     {
-        p_str[i] = c;
-        printf("%c", p_str[i]);
+        p_str[i] = generate_random_simbol(i);
+        // printf("%c", p_str[i]);
     }
-    printf("\n");
+    printf("%s\n", p_str);
 
     free(p_str);
 
     return 0;
+}
+
+char generate_random_simbol(int i)
+{
+    
+    return i == 10 ? 32 :(32 + rand() % (126 - 33));
 }
